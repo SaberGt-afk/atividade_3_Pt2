9b031ef1601ec75430f779bd31f7fd013f478dfd
SaberGt-afk <gtsaber0000@gmail.com>
2024-11-03 16:08:33 +0000

    Tree Printing

--- Assets/scripts/RoberBehaviour.cs
+++ Assets/scripts/RoberBehaviour.cs
@@ -17,6 +17,8 @@ public class RoberBehaviour : MonoBehaviour
         steal.AddChild(goToDiamond);
         steal.AddChild(goToVan);
         tree.AddChild(steal);
+
+        tree.PrintTree();
     }
 
     // Update is called once per frame
